504a639c8015896903d2805f85c428ec31b6ff19
Kunals909 <86119737+Kunals909@users.noreply.github.com>
2022-06-20 21:22:29 +0530

    Update Iterative solution.cpp

--- Linked List/Reverse a linked list/Iterative solution.cpp
+++ Linked List/Reverse a linked list/Iterative solution.cpp
@@ -1,1 +1,23 @@
 
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* reverseList(struct Node *head)
+    {
+       if(head==NULL)
+         return head;
+      Node *curr= head;
+      Node * prev=NULL;
+      while(curr!=NULL){
+          Node* nxt=curr->next;
+          curr->next=prev;
+          prev=curr;
+          curr=nxt;
+      }
+      
+      return prev;
+    }
+
